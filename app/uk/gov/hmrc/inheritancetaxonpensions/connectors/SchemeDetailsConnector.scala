8532088c3b3a19804f85854ef340efdb39f3a04c
ajchilts <40767309+ajchilts@users.noreply.github.com>
2026-02-24 11:18:54 +0000

    IHTP-276 | Remove redundant logging

--- app/uk/gov/hmrc/inheritancetaxonpensions/connectors/SchemeDetailsConnector.scala
+++ app/uk/gov/hmrc/inheritancetaxonpensions/connectors/SchemeDetailsConnector.scala
@@ -38,7 +38,6 @@ class SchemeDetailsConnector @Inject() (appConfig: AppConfig, http: HttpClientV2
     ec: ExecutionContext
   ): Future[Boolean] = {
     val url = appConfig.isPsaAssociatedUrl
-    logger.info(s"[SchemeDetailsConnector][checkAssociation] - calling api: $url")
     http
       .get(url"$url")
       .transform(
