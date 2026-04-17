e5c5d9e1c02abc28bc59ff6b7d95599f67f268d1
Bradleigh-L <189644673+Bradleigh-L@users.noreply.github.com>
2026-04-17 08:18:25 +0100

    IHTP-337: Fix failing test due to rename

--- test/uk/gov/hmrc/inheritancetaxonpensions/services/ReportSubmissionServiceSpec.scala
+++ test/uk/gov/hmrc/inheritancetaxonpensions/services/ReportSubmissionServiceSpec.scala
@@ -70,7 +70,7 @@ class ReportSubmissionServiceSpec extends AnyFreeSpec with Matchers with Mockito
     }
 
     "return Left when connector returns an error" in {
-      val testUserAnswers = UserAnswers(testUserAnswersId, Json.obj("inheritanceTaxReferenceNumber" -> "A123459/25A"))
+      val testUserAnswers = UserAnswers(testUserAnswersId, Json.obj("inheritanceTaxReference" -> "A123459/25A"))
       when(mockUserAnswersRepository.get(testUserAnswersId)).thenReturn(Future.successful(Some(testUserAnswers)))
       when(mockIhtpReportConnector.submitReport(any())(any()))
         .thenReturn(Future.successful(Left(ErrorCodes.badRequest)))
